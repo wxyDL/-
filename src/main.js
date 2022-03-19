859a2aea426087b8dd2d19a7354f5e86af2d4ff2
wangxuying <1926526162@qq.com>
2022-03-19 14:10:18 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入全局样式
+import '../style/index.less'
+// 引入vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
